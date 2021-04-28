1fb293c98492cbcec58ac699405ffa92f7973ed4
doctorstrangedross <82964500+doctorstrangedross@users.noreply.github.com>
2021-04-28 17:10:34 -0500

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,7 +3,7 @@
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 
-	$email_from = 'https://doctorstrangedross.github.io/doctorstrange/';
+	$email_from = 'wolverinezentry@gmail.com';
 	
 	$email_subject = "New Form Submission";
 
